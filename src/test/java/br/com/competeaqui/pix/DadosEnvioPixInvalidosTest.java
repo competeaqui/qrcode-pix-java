be0a967ae2f03e04a9d8ec8fa367135036d5c8e4
Manoel Campos <manoelcampos@gmail.com>
2023-04-02 17:32:30 -0300

    Inclui classes de teste para DadosEnvioPix.java
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ src/test/java/br/com/competeaqui/pix/DadosEnvioPixInvalidosTest.java
@@ -0,0 +1,71 @@
+package br.com.competeaqui.pix;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static br.com.competeaqui.pix.DadosEnvioPixValorTest.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Testes de validação de parâmetros dos construtores de {@link DadosEnvioPix}.
+ *
+ * <p>As constantes posuem nomes abreviados por são irrelevantes para os testes.
+ * Cada teste considera apenas o valor inválido passado por meio de uma variável
+ * (as constantes são valores válidos).</p>
+ *
+ * @author Manoel Campos da Silva Filho
+ */
+class DadosEnvioPixInvalidosTest {
+    private static final BigDecimal V = new BigDecimal(1);
+
+    /** Nome no limite do tamanho máximo. */
+    @Test
+    void nomeDestinatarioNoLimite() {
+        final var nomeInvalido = "a".repeat(25);
+        assertDoesNotThrow(() -> new DadosEnvioPix(nomeInvalido, CD, V, CR));
+    }
+
+    @Test
+    void nomeDestinatarioMuitoGrande() {
+        final var nomeInvalido = "a".repeat(26);
+        assertThrows(IllegalArgumentException.class, () -> new DadosEnvioPix(nomeInvalido, CD, V, CR));
+    }
+
+
+    /** Valor no limite do tamanho máximo. */
+    @Test
+    void valorNoLimite() {
+        final var valorInvalido = new BigDecimal(1234567890.00); // 13 caracteres (com o ponto)
+        assertDoesNotThrow(() -> new DadosEnvioPix(ND, CD, valorInvalido, CR));
+    }
+
+    /** Quando o total de caracteres do valor (incluíndo o ponto) é maior do que o suportado. */
+    @Test
+    void valorDoubleMuitoGrande() {
+        final var valorInvalido = new BigDecimal(12345678901.00); // 14 caracteres (com o ponto)
+        assertThrows(IllegalArgumentException.class, () -> new DadosEnvioPix(ND, CD, valorInvalido, CR));
+    }
+
+    /** Quando o total de caracteres do valor (incluíndo o ponto) é maior do que o suportado. */
+    @Test
+    void valorIntMuitoGrande() {
+        // 11 caracteres, mas neste caso será incluído .00 ficando com 14 (além do limite)
+        final var valorInvalido = new BigDecimal("12345678901");
+        assertThrows(IllegalArgumentException.class, () -> new DadosEnvioPix(ND, CD, valorInvalido, CR));
+    }
+
+    /** Cidade no limite do tamanho máximo. */
+    @Test
+    void cidadeRemetenteNoLimite() {
+        final var cidadeInvalida = "a".repeat(15);
+        assertDoesNotThrow(() -> new DadosEnvioPix(ND, CD, V, cidadeInvalida));
+    }
+
+    @Test
+    void cidadeRemetenteMuitoGrande() {
+        final var cidadeInvalida = "a".repeat(16);
+        assertThrows(IllegalArgumentException.class, () -> new DadosEnvioPix(ND, CD, V, cidadeInvalida));
+    }
+}
---
+++ src/test/java/br/com/competeaqui/pix/DadosEnvioPixValorTest.java
@@ -0,0 +1,69 @@
+package br.com.competeaqui.pix;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Testes para o campo {@link DadosEnvioPix#valor()}.
+ *
+ * <p>As constantes posuem nomes abreviados por são irrelevantes para os testes.
+ * Cada teste considera apenas o valor inválido passado por meio de uma variável
+ * (as constantes são valores válidos).</p>
+ *
+ * @author Manoel Campos da Silva Filho
+ */
+class DadosEnvioPixValorTest {
+    static final String ND = "Manoel";
+    static final String CD = "11111111111";
+    static final String CR = "Palmas";
+
+    @Test
+    void valorStr1Casa() {
+        final var instance = newInstance(new BigDecimal(1.0));
+        assertEquals("1.00", instance.valorStr());
+    }
+
+    @Test
+    void valorStr2Casas() {
+        final var instance = newInstance(new BigDecimal(1.00));
+        assertEquals("1.00", instance.valorStr());
+    }
+
+    @Test
+    void valorStr3Casas() {
+        final var instance = newInstance(new BigDecimal(1.234));
+        assertEquals("1.23", instance.valorStr());
+    }
+
+    @Test
+    void valorStr3CasasZerosNoMeio() {
+        final var instance = newInstance(new BigDecimal(1.001));
+        assertEquals("1.00", instance.valorStr());
+    }
+
+    @Test
+    void valorStr3CasasZeroFinal() {
+        final var instance = newInstance(new BigDecimal(1.230));
+        assertEquals("1.23", instance.valorStr());
+    }
+
+    /** Teste de arredondamento pra cima. */
+    @Test
+    void valorStrMaisDe2CasasCeil() {
+        final var instance = newInstance(new BigDecimal(1.229));
+        assertEquals("1.23", instance.valorStr());
+    }
+
+    @Test
+    void valorStrMaisDe2CasasZeros() {
+        final var instance = newInstance(new BigDecimal(1.000));
+        assertEquals("1.00", instance.valorStr());
+    }
+
+    private static DadosEnvioPix newInstance(final BigDecimal valor) {
+        return new DadosEnvioPix(ND, CD, valor, CR);
+    }
+}
